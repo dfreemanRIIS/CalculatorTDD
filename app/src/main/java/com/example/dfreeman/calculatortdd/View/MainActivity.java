f9e29943c7dda64d58c83a14c7f428ce4c3ac176
dfreemanRIIS <dfreeman@riis.com>
2017-02-22 14:27:54 -0500

    3 - UI Content Feature TEST SUCCESS

--- app/src/main/java/com/example/dfreeman/calculatortdd/View/MainActivity.java
+++ app/src/main/java/com/example/dfreeman/calculatortdd/View/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.dfreeman.calculatortdd.View;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.dfreeman.calculatortdd.R;
 
@@ -11,5 +12,56 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickZero(View v) {
+    }
+
+    public void onClickOne(View v) {
+    }
+
+    public void onClickTwo(View v) {
+    }
+
+    public void onClickThree(View v) {
+    }
+
+    public void onClickFour(View v) {
+    }
+
+    public void onClickFive(View v) {
+    }
+
+    public void onClickSix(View v) {
+    }
+
+    public void onClickSeven(View v) {
+    }
+
+    public void onClickEight(View v) {
+    }
+
+    public void onClickNine(View v) {
+    }
+
+    public void onPlusClick(View v) {
+    }
+
+    public void onMinusClick(View v) {
+    }
+
+    public void onMultClick(View v) {
+    }
+
+    public void onDivideClick(View v) {
+    }
+
+    public void onClickClear(View v) {
+    }
+
+    public void onEqualsClick(View v) {
+    }
+
+    public void onChangeColorClick(View v) {
     }
 }
